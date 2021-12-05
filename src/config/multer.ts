e0f7fe9816968b9ada82a68eeeb18c3c3af50065
Vinicius Queiroz <viniderp@gmail.com>
2021-12-05 00:15:23 -0300

    multer config

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,8 +1,29 @@
-import { Request } from 'express';
 import multer from 'multer';
+import crypto from 'crypto';
+
+import { Request } from 'express';
+import { AppError } from '../@types/errors/AppError';
+
+interface FileWithKey extends Express.Multer.File {
+  key: string;
+}
 
 export const multerConfig: multer.Options = {
   dest: "../../tmp/uploads",
+  storage: multer.diskStorage({
+    destination: (req: Request, file, cb) => {
+      cb(null, "../../tmp/uploads");
+    },
+    filename: (req: Request, file: FileWithKey, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err, file.originalname);
+  
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+        
+        cb(null, file.key);
+      });
+    }
+  }),
   fileFilter: (req: Request, file: any, cb: multer.FileFilterCallback) => {
     const mimetypes = [
       "image/jpeg",
@@ -13,6 +34,8 @@ export const multerConfig: multer.Options = {
 
     if (mimetypes.includes(file.mimetype)) {
       cb(null, true);
+    } else {
+      cb(new AppError(422, 'Invalid file format'));
     }
   },
   limits: {
