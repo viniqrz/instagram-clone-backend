17cfb45eb71bb8e4c60a138b69236977b7fa3f3e
Vinicius Queiroz <viniderp@gmail.com>
2021-12-05 00:42:05 -0300

    User Model

---
+++ src/@types/models/User.ts
@@ -0,0 +1,12 @@
+import { Schema } from "mongoose";
+
+export interface User {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  avatar?: string;
+  posts: Schema.Types.ObjectId[];
+  followers: Schema.Types.ObjectId[];
+  following: Schema.Types.ObjectId[];
+}
---
+++ src/models/User.ts
@@ -0,0 +1,44 @@
+import validator from "validator";
+
+import { model, Schema } from "mongoose";
+import { User } from "../@types/models/User";
+
+const userSchema = new Schema<User>({
+  name: {
+    type: String,
+    required: [true, "User must have a name"],
+    maxlength: [60, "Name must be shorter than 61 characters"]
+  },
+  username: {
+    type: String,
+    required: [true, "User must have an username"],
+    maxlength: [20, "Username must be shorter than 21 characters"]
+  },
+  email: {
+    type: String,
+    required: [true, "User must have an email"],
+    maxlength: [80, "Email must be shorter than 21 characters"],
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "User must have a password"],
+  },
+  avatar: String,
+  posts: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Post',
+  },
+  followers: {
+    type: [Schema.Types.ObjectId],
+    ref: 'User',
+  },
+  following: {
+    type: [Schema.Types.ObjectId],
+    ref: 'User',
+  },
+});
+
+const UserModel = model<User>("User", userSchema);
+
+export { UserModel };
