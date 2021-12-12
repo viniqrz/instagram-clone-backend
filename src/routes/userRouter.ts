3f66bceff3a3f25732ea3b7de6ce2e76a2190e39
Vinicius <viniderp@gmail.com>
2021-12-12 17:27:20 -0300

    GetUser Route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -6,6 +6,7 @@ import { AuthenticateController } from '../useCases/Authenticate/AuthenticateCon
 import { ListUsersController } from '../useCases/ListUsers/ListUsersController';
 import { FollowController } from '../useCases/Follow/FollowController';
 import { SignUpController } from '../useCases/SignUp/SignUpController';
+import { GetUserController } from '../useCases/GetUser/GetUserController';
 
 const router = Router();
 
@@ -13,18 +14,18 @@ const signUpController = new SignUpController();
 const authenticateController = new AuthenticateController();
 const listUsersController = new ListUsersController();
 const followController = new FollowController();
+const getUserController = new GetUserController();
 
 router.get('/', ensureAuth, listUsersController.handle);
+router.get('/:id', getUserController.handle);
 
 router.post('/:id/follow', ensureAuth, followController.handle);
-
 router.post(
   '/',
   body(['password', 'email', 'username', 'name']).exists(),
   handleValidationErrors,
   signUpController.handle
 );
-
 router.post(
   '/authenticate',
   body(['password', 'username']).exists(),
--- src/useCases/GetUser/GetUserUseCase.ts
+++ src/useCases/GetUser/GetUserUseCase.ts
@@ -8,7 +8,13 @@ export class GetUserUseCase {
     const isIdValid = isValidObjectId(id);
     if (!isIdValid) throw new AppError(400, 'Id is not valid');
 
-    const user = await UserModel.findById(id).populate({ path: 'posts' });
+    const user = await UserModel.findOne(
+      { _id: id },
+      { following: 0, followers: 0, password: 0 }
+    ).populate({
+      path: 'posts',
+      select: '_id file createdAt',
+    });
     if (!user) throw new AppError(404, 'User not found');
 
     return user;
