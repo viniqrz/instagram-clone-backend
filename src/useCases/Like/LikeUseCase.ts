58eba853b85f5a8be10a5a14203b61fc7bebe7e4
Vinicius <viniderp@gmail.com>
2021-12-13 12:09:22 -0300

    Like useCase

---
+++ src/useCases/Like/LikeUseCase.ts
@@ -0,0 +1,20 @@
+import mongoose, { ObjectId } from 'mongoose';
+
+import { PostModel } from '../../models/Post';
+import { AppError } from '../../@types/errors/AppError';
+import { Post } from '../../@types/models/Post';
+
+export class ListLikesUseCase {
+  public async execute(postId: string, userId: ObjectId): Promise<Post> {
+    const isIdValid = mongoose.isValidObjectId(postId);
+    if (!isIdValid) throw new AppError(400, 'Post id is not valid');
+
+    const post = await PostModel.findById(postId);
+    if (!post) throw new AppError(404, 'Post not found');
+
+    post.likes.push(userId);
+    post.save();
+
+    return post;
+  }
+}
