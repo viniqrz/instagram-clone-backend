f8422c487d41fab21dbeee6fe43e640ab57fe769
Vinicius <viniderp@gmail.com>
2021-12-13 11:01:34 -0300

    ListFollowers Controller

---
+++ src/useCases/ListFollowers/ListFollowersController.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { catchAsync } from '../../helpers/catchAsync';
+import { ListFollowersUseCase } from './ListFollowersUseCase';
+
+export class ListFollowersController {
+  constructor() {
+    this.handle = catchAsync(this.handle);
+  }
+
+  public async handle(
+    req: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction
+  ): Promise<void> {
+    const followers = await new ListFollowersUseCase().execute(
+      req.params.postId
+    );
+
+    res.json({
+      status: 'success',
+      data: followers,
+    });
+  }
+}
