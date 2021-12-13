76458c9718fb330251689800ca1d1aaf4b9e3c7b
Vinicius <viniderp@gmail.com>
2021-12-13 11:53:13 -0300

    ListLikes useCase

---
+++ src/useCases/ListLikes/ListLikesUseCase.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+import { PostModel } from '../../models/Post';
+import { AppError } from '../../@types/errors/AppError';
+
+export class ListLikesUseCase {
+  public async execute(postId: string) {
+    const isIdValid = mongoose.isValidObjectId(postId);
+    if (!isIdValid) throw new AppError(400, 'Post id is not valid');
+
+    const post = await PostModel.findOne({ _id: postId }, { followers: 1 });
+
+    return post;
+  }
+}
