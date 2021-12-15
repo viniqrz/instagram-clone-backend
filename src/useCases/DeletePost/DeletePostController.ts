68b8f1ee374a3d60a244c45e445eba98e1d3e353
Vinicius <viniderp@gmail.com>
2021-12-14 21:39:01 -0300

    DeletePost Controller

---
+++ src/useCases/DeletePost/DeletePostController.ts
@@ -0,0 +1,24 @@
+import { Response, NextFunction } from 'express';
+import { IRequest } from '../../@types/express/IRequest';
+import { catchAsync } from '../../helpers/catchAsync';
+import { DeletePostUseCase } from './DeletePostUseCase';
+
+export class DeletePostController {
+  constructor() {
+    this.handle = catchAsync(this.handle);
+  }
+
+  public async handle(
+    req: IRequest,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction
+  ): Promise<void> {
+    await new DeletePostUseCase().execute(req.params.postId, req.user._id);
+
+    res.json({
+      status: 'success',
+      message: 'Post deleted succesfully!',
+    });
+  }
+}
