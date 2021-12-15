96f3d4b13d71cad94d16b66512e107e361101471
Vinicius <viniderp@gmail.com>
2021-12-14 22:06:09 -0300

    UpdateMe password hash fix

--- src/useCases/UpdateMe/UpdateMeUseCase.ts
+++ src/useCases/UpdateMe/UpdateMeUseCase.ts
@@ -1,8 +1,12 @@
+import { hash } from 'bcrypt';
 import { PartialUser } from '../../@types/dtos/UserDto';
 import { UserModel } from '../../models/User';
 
 export class UpdateMeUseCase {
   public async execute(userId: string, partial: PartialUser) {
+    if ('password' in partial)
+      partial.password = await hash(partial.password, 8);
+
     await UserModel.findByIdAndUpdate(userId, partial);
   }
 }
